7c0db3f7c72f0a2bad425497f983784c74d9ef4e
Baris <karalarbaris@gmail.com>
2021-12-05 18:31:06 +0100

    Adding custom controllers

---
+++ 1 - Storm Viewer macos/DetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.swift
+//  1 - Storm Viewer macos
+//
+//  Created by Baris Karalar on 5.12.2021.
+//
+
+import Cocoa
+
+class DetailViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
---
+++ 1 - Storm Viewer macos/SourceViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SourceViewController.swift
+//  1 - Storm Viewer macos
+//
+//  Created by Baris Karalar on 5.12.2021.
+//
+
+import Cocoa
+
+class SourceViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
