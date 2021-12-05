d131f40f84e49d2a1f9288112b38b86e848ac5ef
Baris <karalarbaris@gmail.com>
2021-12-05 14:08:23 +0100

    Initial Commit

---
+++ 1 - Storm Viewer macos/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  1 - Storm Viewer macos
+//
+//  Created by Baris Karalar on 5.12.2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
