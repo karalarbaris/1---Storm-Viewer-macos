1bf408591460c776b2b4a37d4e4fb934bc510178
Baris <karalarbaris@gmail.com>
2021-12-06 11:22:51 +0100

    Showing some table rows

--- 1 - Storm Viewer macos/SourceViewController.swift
+++ 1 - Storm Viewer macos/SourceViewController.swift
@@ -18,3 +18,23 @@ class SourceViewController: NSViewController {
     }
     
 }
+
+extension SourceViewController: NSTableViewDataSource {
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return 22
+    }
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        guard let vw = tableView.makeView(withIdentifier: tableColumn!.identifier, owner: self) as? NSTableCellView else {
+            return nil
+        }
+        
+        vw.textField?.stringValue = "halloooo!!!"
+        
+        return vw
+    }
+}
+
+extension SourceViewController: NSTableViewDelegate {
+    
+}
