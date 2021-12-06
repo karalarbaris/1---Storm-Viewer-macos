88df099329befb234c40c2a2afaf6844ba1b1919
Baris <karalarbaris@gmail.com>
2021-12-06 11:36:32 +0100

    Loading our images

--- 1 - Storm Viewer macos/SourceViewController.swift
+++ 1 - Storm Viewer macos/SourceViewController.swift
@@ -10,18 +10,34 @@ import Cocoa
 class SourceViewController: NSViewController {
 
     @IBOutlet var tableView: NSTableView!
+    var pictures = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do view setup here.
         
+        addImageNamesToPicturesArr()
+        
+        
+    }
+    
+    func addImageNamesToPicturesArr() {
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+            
+        }
     }
     
 }
 
 extension SourceViewController: NSTableViewDataSource {
     func numberOfRows(in tableView: NSTableView) -> Int {
-        return 22
+        return pictures.count
     }
     
     func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
@@ -29,7 +45,7 @@ extension SourceViewController: NSTableViewDataSource {
             return nil
         }
         
-        vw.textField?.stringValue = "halloooo!!!"
+        vw.textField?.stringValue = pictures[row]
         
         return vw
     }
