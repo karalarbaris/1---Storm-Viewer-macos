c588abc126b59d06788ffde5dc92f4bca5f07bc5
Baris <karalarbaris@gmail.com>
2021-12-23 12:53:49 +0300

    Test2

--- 1 - Storm Viewer macos/ViewController.swift
+++ 1 - Storm Viewer macos/ViewController.swift
@@ -22,6 +22,7 @@ class ViewController: NSSplitViewController {
         let picker = NSSharingServicePicker(items: [image])
         picker.show(relativeTo: .zero, of: sender, preferredEdge: .minY)
         
+        
     }
     
 
